a661a73d39a98f3e6209bffb26c82a8f8714b4b7
Philme1 <philipinoo28@gmail.com>
2020-07-13 16:35:16 +0100

    Added ColorHelper.js and the color slider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Palette from './Palette';
 import seedColors from "./seedColors";
+import { Component } from 'react';
+import { generatePalette } from './colorHelpers';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-     <Palette {...seedColors[4]} />
+     <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
+  }
 }
 
 export default App;
