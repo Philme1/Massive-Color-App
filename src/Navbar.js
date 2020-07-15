0474d5384da38ee34d90c5bd0635386e7788efce
Philme1 <philipinoo28@gmail.com>
2020-07-15 16:34:32 +0100

    Move styles into Styles directory and refactor Navbar styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Select from '@material-ui/core/Select';
 import Slider from "rc-slider";
-
+import { MenuItem, Snackbar, IconButton } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
 // import Snackbar from '@material-ui/core/Snackbar';
 // import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
 
 import 'rc-slider/assets/index.css';
-import "./Navbar.css"; 
-import { MenuItem, Snackbar, IconButton } from '@material-ui/core';
+import styles from "./styles/NavbarStyles";
+
 
 
 class Navbar extends Component {
@@ -31,17 +32,17 @@ class Navbar extends Component {
   }
 
   render() {
-    const { level, changeLevel, showingFullPalette } = this.props;
+    const { level, changeLevel, showingAllColors, classes } = this.props;
     const { format } = this.state;
     return (
-      <header className="Navbar">
-          <div className="logo">
+      <header className={classes.Navbar}>
+          <div className={classes.logo}>
             <Link to="/">reactcolorpicker</Link>
           </div>
-          {showingFullPalette && (
-            <div className="slider-container">
+          {showingAllColors && (
+            <div>
             <span>Level: {level} </span>
-             <div className="slider">
+             <div className={classes.slider}>
                <Slider 
                  defaultValue={level}
                  min={100}
@@ -49,11 +50,11 @@ class Navbar extends Component {
                  step={100}
                  onAfterChange={changeLevel}
                />
-               </div>
+              </div>
           </div>
           )}
        
-       <div className="select-container">
+       <div className={classes.selectContainer}>
          <Select value={format} onChange={this.handleFormatChange}>
           <MenuItem value="hex">HEX - #ffffff</MenuItem>
           <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
@@ -78,4 +79,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import ColorBox from "./ColorBox";
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+
+import styles from "./styles/PaletteStyles";
+import ColorBox from "./ColorBox";
+
+
 
 class SingleColorPalette extends Component {
   constructor(props) {
@@ -33,6 +38,7 @@ class SingleColorPalette extends Component {
   render() {
     const { format } = this.state;
     const { paletteName, emoji, id } = this.props.palette;
+    const { classes } = this.props;
     const colorBoxes = this._shades.map(color => (
       <ColorBox
         key={color.name}
@@ -42,12 +48,12 @@ class SingleColorPalette extends Component {
       />
     ));
     return (
-      <div className="SingleColorPalette Palette">
+      <div className={classes.Palette}>
         <Navbar handleChange={this.changeFormat} showingAllColors={false} />
-        <div className="Palette-colors">
+        <div className={classes.colors}>
           {colorBoxes}
-        <div className="go-back ColorBox">
-          <Link to={`/palette/${id}`} className="back-button"> Go Back </Link>
+        <div className={classes.goBack}>
+          <Link to={`/palette/${id}`}> Go Back </Link>
         </div>
         </div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
@@ -56,4 +62,4 @@ class SingleColorPalette extends Component {
   }
 }
 
-export default SingleColorPalette;
+export default withStyles(styles)(SingleColorPalette);
